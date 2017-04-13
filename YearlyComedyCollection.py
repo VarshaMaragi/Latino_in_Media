91c1ad78adf1973e2d60deb711e81790c854eaa2
VarshaMaragi <varshamaragi@gmail.com>
2017-04-12 22:23:05 -0400

    Fixed the i error

--- YearlyComedyCollection.py
+++ YearlyComedyCollection.py
@@ -27,6 +27,8 @@ def getmoviesbyyear():
 	dcode["documentary"]=99
 	dcode["drama"]=18
 	wikipedialist=dict()
+	overviewlist=list()
+	foverview=open('movieoverview.txt','a')
 	LA_COUNTRIES=re.compile(".*Cuba.*|.*Dominican Republic.*|.*Puerto Rico.*|.*Costa Rica.*|.*El Salvador.*|.*Guatemala.*|.*Mexico.*|.*Nicaragua.*|.*Panama.*|.*Argentina.*|.*Nicaragua.*|.*Bolivia.*|.*Chile.*|.*Colombia.*|.*Ecuador.*|.*Guyana.*|.*Paraguay.*|.*Peru.*|.*Uruguay.*|.*Venezuela.*")
 	LA_NAMES=re.compile(".*ez$|.*do$|.*ro$")
 	year=raw_input("Enter year: ")
@@ -35,40 +37,46 @@ def getmoviesbyyear():
 	pc=[1,2,3,4,5,6,7,8,9,11,12,13,14,16,17,25,8411]
 	for productioncompany in pc:
 		print("production compnay number",productioncompany)
-		url="https://api.themoviedb.org/3/discover/movie?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&primary_release_year="+str(year)+"&with_genres=35&year="+str(year)+"&without_genres=28,12,16,18,80,99,18,10751,14,36,27,10402,9648,10749,878,10770,53,37,10752&with_original_language=en"
+		url="https://api.themoviedb.org/3/discover/movie?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&primary_release_year="+str(year)+"&with_genres=35&year="+str(year)+"&without_genres=28,12,16,18,80,99,18,10751,14,36,27,10402,9648,10749,878,10770,53,37,10752&with_original_language=en&certification_country=US&with_companies="+str(productioncompany)
 		urllib.urlretrieve (url,"year2017.json");
-		first_one = True
 		with open('year2017.json') as data_file:
 			data = json.load(data_file)
-			print data["total_pages"]
+			print("Total Pages",data["total_pages"])
+			f=open('Comedy'+str(year)+'Cast.csv', 'a')
+			g=open('Comedy'+str(year)+'Crew.csv','a')
 			for i in range(1,data["total_pages"]+1):
 				print "Page",i
 				print "-------------------------------------------"
 				time.sleep(3)
-				url="https://api.themoviedb.org/3/discover/movie?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page="+str(i)+"&primary_release_year="+str(year)+"&with_genres=35&year="+str(year)+"&without_genres=28,12,16,18,80,99,18,10751,14,36,27,10402,9648,10749,878,10770,53,37,10752&with_original_language=en"
+				url="https://api.themoviedb.org/3/discover/movie?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page="+str(i)+"&primary_release_year="+str(year)+"&with_genres=35&year="+str(year)+"&without_genres=28,12,16,18,80,99,18,10751,14,36,27,10402,9648,10749,878,10770,53,37,10752&with_original_language=en&certification_country=US&with_companies="+str(productioncompany)
 				urllib.urlretrieve (url,"year2017.json");
-		
+			
+
 				with open('year2017.json') as data_file2:
 					data2 = json.load(data_file2)
 					print "data2"
 					print data2
 					if "results" in data2.keys():
-						print "in result"
+						print "in results"
+						for i in data2["results"]:
+							if i.get("original_language")=="en":
 
 								print i.get("original_title")
-								f=open('Comedy'+str(year)+'Cast.csv', 'a')
-								g=open('Comedy'+str(year)+'Crew.csv','a')
+							
 								s=(i.get("original_title").encode('utf-8','ignore').decode('utf-8'))
-								if first_one:
-									first_one = False
-								else:
-									f.write("//Movie title//\n")
-									g.write("//Movie title//\n")
+								#s=unicode(s.strip(codecs.BOM_UTF8), 'utf-8')
+								f.write("//Movie title//\n")
 								f.write(s)
+								g.write("//Movie title//\n")
 								g.write(s)
 								rescast=i.get("id")
 								print str(rescast)
 								overviewmovie=i.get("overview")
+								print("overview",str(overviewmovie))
+								overviewlist.append(str(overviewmovie))
+								foverview.write(overviewmovie)
+								foverview.write("\n")
+								foverview.write("\n")
 								if LA_COUNTRIES.findall(overviewmovie) or LA_NAMES.findall(overviewmovie):
 									for startwithcap in LA_COUNTRIES.findall(overviewmovie):
 										if startwithcap.istitle():
@@ -108,14 +116,20 @@ def getmoviesbyyear():
 											imdbid=""
 											print j.get("name")
 											tmdbid=j.get("id")
+											time.sleep(1)
 											urlimdb="https://api.themoviedb.org/3/person/"+str(tmdbid)+"/external_ids?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c&language=en-US"
 											urllib.urlretrieve(urlimdb,"./actorimdb.json")
 											with open('actorimdb.json') as data_fileimdb:
 												temp=json.load(data_fileimdb)
 												if "imdb_id" in temp.keys():
 													imdbid=temp["imdb_id"]
+												if "facebook_id" in temp.keys():
+													facebook_id=temp["facebook_id"]
+													print("Facebook id",facebook_id)
 											if j.get("order")<=2:
 												genlead=genlead+1
+												print("overview changed")
+												overviewlist[-1]=overviewmovie+str(j.get("name"))
 											else:
 												gensupport=gensupport+1
 											if LA_NAMES.findall(j.get("name")):
@@ -162,6 +176,8 @@ def getmoviesbyyear():
 															#wikipedialist[1].append([str(j.get("name")),imdbid])
 															wikipedialist[imdbid]=[str(j.get("name")),pob]
 
+														#wikipedialist.append((j.get("name"),imdbid))
+
 												print "pob",pob
 
 												if "profile_path" in dataperson.keys():
@@ -179,7 +195,7 @@ def getmoviesbyyear():
 											if LA_NAMES.findall(j.get("name")):
 												attachnamebased="Latinx"
 											#strtemp2=strtemp2+j.get("name").encode('utf-8')+"\t\t"+j.get("job").encode('utf-8')+"\n"
-											#time.sleep(1)
+											time.sleep(1)
 											url3="https://api.themoviedb.org/3/person/"+str(j.get("id"))+"?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c&language=en-US"
 											urllib.urlretrieve(url3,"./placeofbirth.json")
 											pob=""
@@ -216,21 +232,25 @@ def getmoviesbyyear():
 								f.write("\n")
 		
 								g.write("\n")
-			#print(totalactors)
-			#print(latinoactors)
-			f.write(str(totalactors))
-			f.write("\n")
-			f.write(str(latinoactors))
-			f.write("\n")
-			f.write(str(latinolead))
-			f.write("\n")
-			f.write(str(genlead))
-			f.write("\n")
-			f.write(str(gensupport))
-			f.write("\n")
-			f.write(str(latinosupport))
-			print(wikipedialist)
-			return wikipedialist
+							
+		#print(totalactors)
+		#print(latinoactors)
+		f.write(str(totalactors))
+		f.write("\n")
+		f.write(str(latinoactors))
+		f.write("\n")
+		f.write(str(latinolead))
+		f.write("\n")
+		f.write(str(genlead))
+		f.write("\n")
+		f.write(str(gensupport))
+		f.write("\n")
+		f.write(str(latinosupport))
+		if overviewlist:
+			print("overviewlist",overviewlist)
+			#MovieClassification.classifyLandNL(overviewlist)
+		print(wikipedialist)
+		
 
 
 
