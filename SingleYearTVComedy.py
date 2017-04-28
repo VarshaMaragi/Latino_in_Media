47cbf907996639fc8f4530d0380116b09750338e
deliabullock <delia@textiq.com>
2017-04-28 15:14:59 -0400

    adding tv shows crew episode numbers

--- SingleYearTVComedy.py
+++ SingleYearTVComedy.py
@@ -17,6 +17,9 @@ class Actor(object):
 		#self.latino = False
 		self.role = role
 		self.name = name
+		self.start = "0"
+		self.end = "0"
+		self.epi_num = ""
 		#num_epi = ""
 		#years = ""
 		
@@ -31,13 +34,17 @@ class Crew(object):
 		self.co_exec_producers = []
 		self.misc = []
 
-	def add_crew_member(self, name, role):
-		if role == "Executive Producer" or role == "Co-Executive Producer":
-			self.exec_producers.append([name, role])
+	def add_crew_member(self, name, role, epi_info):
+		if role == "Director":
+			self.directors.append([name, role, epi_info])
+		elif role == "Writer":
+			self.creators.append([name, role, epi_info])
+		elif role == "Executive Producer" or role == "Co-Executive Producer":
+			self.exec_producers.append([name, role, epi_info])
 		elif "roducer" in role:
-			self.producers.append([name, role])
+			self.producers.append([name, role, epi_info])
 		else:
-			self.misc.append([name, role])
+			self.misc.append([name, role, epi_info])
 
 def getTVdetailsby_year(year):
 	cast_file=open(str(year) + "TVComedyCast.csv",'a')
@@ -78,7 +85,7 @@ def getTVdetailsby_year(year):
 							break
 					if not (keep_tv):
 						continue
-					cast_file.write(" ;"+str(i.get("original_name"))+'\n')
+					cast_file.write(str(i.get("original_name"))+'\n')
 					crew_file.write(str(i.get("original_name"))+'\n')
 
 					time.sleep(3)
@@ -86,15 +93,43 @@ def getTVdetailsby_year(year):
 					urllib.request.urlretrieve (url2,"./cast2017.json");
 					with open('cast2017.json') as data_file2:
 						datac = json.load(data_file2)
+						actors = {}
+						crew =  {}
 						if "cast" in datac.keys():
 							for j in datac["cast"]:
 								a = Actor(str(j.get("name")), str(j.get("character")))
-								write_actor(cast_file, a)
+								actors[str(j.get("character"))] = [a, 0, 6000, -1]
 						if "crew" in datac.keys():
-							crew = Crew()
 							for j in datac["crew"]:
-								crew.add_crew_member(str(j.get("name")), str(j.get("job")))
-							write_crew(crew_file, crew)
+								crew[str(j.get("name")) + ";" + str(j.get("job"))] = [0, 6000, -1]
+						actors, crew = get_episode_counts(i.get("id"), actors, crew)
+						sorted_actors = [actors[k] for k in actors]
+						def get_epi_num(x):
+							if len(x) < 2:
+								return 0
+							if x[1] == None or x[1] == "":
+								return 0
+							return x[1]
+						sorted_actors = sorted(sorted_actors, key=get_epi_num, reverse=True) 
+						for info in sorted_actors:
+							a = info[0]
+							a.epi_num = info[1]
+							a.start = info[2]
+							a.end = info[3]
+							write_actor(cast_file, a)
+						sorted_crew = [[k.split(";")[0], k.split(";")[1], crew[k]] for k in crew]
+						def get_epi_num_crew(x):
+							if x[2][0] == None or x[2][0] == "":
+								return 0
+							return x[2][0]
+						sorted_crew = sorted(sorted_crew, key=get_epi_num_crew, reverse=True) 
+						crew_obj = Crew()
+						for info in sorted_crew:
+							name = info[0]
+							role = info[1]
+							epi_info = info[2]
+							crew_obj.add_crew_member(name, role, epi_info)
+						write_crew(crew_file, crew_obj)
 					cast_file.write("\n")
 					crew_file.write("\n")
 
@@ -103,25 +138,90 @@ def getTVdetailsby_year(year):
 		cast_file.close()
 		crew_file.close()
 
+def get_episode_counts(_id, cast, crew):
+	episode_list = get_episodes_dict(_id)
+	for x in episode_list:
+		seas_num = x[0]
+		epi_num_list = x[1]
+		air_date = x[2]
+		for epi_num in range(1, epi_num_list):
+			time.sleep(3)
+			url = "https://api.themoviedb.org/3/tv/" + str(_id) + "/season/" + str(seas_num) + "/episode/" + str(epi_num) + "/credits?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c"
+			print(url)
+			urllib.request.urlretrieve (url,"./episode.json");
+			with open('episode.json') as data_file2:
+				datac = json.load(data_file2)
+				if "cast" in datac.keys():
+					for j in datac["cast"]:
+						if str(j.get("character")) in cast:
+							cast[str(j.get("character"))][1] = cast[str(j.get("character"))][1] + 1
+							if air_date < cast[str(j.get("character"))][2]:
+								cast[str(j.get("character"))][2] = air_date
+							if air_date > cast[str(j.get("character"))][3]:
+								cast[str(j.get("character"))][3] = air_date
+						else:
+							a = Actor(str(j.get("name")), str(j.get("character")))
+							cast[str(j.get("character"))] = [a, 1, air_date, air_date]
+				if "guest_stars" in datac.keys():
+					for j in datac["guest_stars"]:
+						if str(j.get("character")) in cast:
+							cast[str(j.get("character"))][1] = cast[str(j.get("character"))][1] + 1
+							if air_date < cast[str(j.get("character"))][2]:
+								cast[str(j.get("character"))][2] = air_date
+							if air_date > cast[str(j.get("character"))][3]:
+								cast[str(j.get("character"))][3] = air_date
+						else:
+							a = Actor(str(j.get("name")), str(j.get("character")))
+							cast[str(j.get("character"))] = [a, 1, air_date, air_date]
+				if "crew" in datac.keys():
+					for j in datac["crew"]:
+						if str(str(j.get("name")) + ";" + str(j.get("job"))) in crew:
+							crew[str(j.get("name")) + ";" + str(j.get("job"))][0] =  crew[str(j.get("name")) + ";" + str(j.get("job"))][0] + 1
+							if air_date < crew[str(j.get("name")) + ";" + str(j.get("job"))][1]:
+								crew[str(j.get("name")) + ";" + str(j.get("job"))][1] = air_date
+							if air_date > crew[str(j.get("name")) + ";" + str(j.get("job"))][2]:
+								crew[str(j.get("name")) + ";" + str(j.get("job"))][2] = air_date
+						else:
+							crew[str(j.get("name")) + ";" + str(j.get("job"))] = [1, air_date, air_date]
+	return cast, crew					
+
+def get_episodes_dict(_id):
+	seasons = []
+	time.sleep(3)
+	url = "https://api.themoviedb.org/3/tv/" + str(_id) + "?api_key=17ce03ebb1e89f2dcf4eec0e9c2b8e6c&language=en-US"
+	urllib.request.urlretrieve (url,"./episode.json");
+	with open('episode.json') as data_file2:
+		datac = json.load(data_file2)
+		if "seasons" in datac.keys():
+			print(datac["seasons"])
+			for j in datac["seasons"]:
+				air_date = -1
+				if j.get("air_date") != None:
+					air_date = int(j.get("air_date")[:4])
+				seasons.append([int(j.get("season_number")), int(j.get("episode_count")), air_date])
+	return seasons
+	
+
 def write_actor(f, actor):
 	s=""
-	s=';;'+actor.name+";"+actor.role
+	s=';'+str(actor.name)+";;;;;;"+str(actor.name)+';'+str(actor.role)+';'+str(actor.epi_num)+';'+str(actor.start)+'-'+str(actor.end)+';;;;;;;;;'+str(actor.name)+';'+str(actor.role)+';'
 	f.write(s)
 	f.write("\n")
 
 def write_crew(f, crew):
-	max_len = max(len(crew.misc), max(max(len(crew.producers), len(crew.directors)), max(len(crew.exec_producers), len(crew.co_exec_producers))))
-	l = [["",""]]*max_len
+	max_len = max(max(len(crew.misc), len(crew.creators)), max(max(len(crew.producers), len(crew.directors)), max(len(crew.exec_producers), len(crew.co_exec_producers))))
+	l = [["","",["","",""]]]*max_len
 	crew_lists = [list(l), list(l), list(l), list(l), list(l)] 
 	k = [crew.creators, crew.directors, crew.producers, crew.exec_producers, crew.misc]
 	for j in range(len(k)):
 		for i in range(len(k[j])):
 			crew_lists[j][i] = k[j][i]
-	print(crew_lists)
-	print()
-	print()
 	for i in range(max_len):
-		s = ";" + crew_lists[0][i][0] + ";;;;;;;;" + crew_lists[1][i][0] + ";;;;;;;;" + crew_lists[2][i][0] + ";" + crew_lists[2][i][1] +  ";;;;;;;;" + crew_lists[3][i][0] + ";" + crew_lists[3][i][1] +  ";;;;;;;;" + crew_lists[4][i][0] + ";" + crew_lists[4][i][1] + ";;;;;;;;"  
+		s = ";" + crew_lists[0][i][0] + ";;;;;"+str(crew_lists[0][i][2][0])+";"+str(crew_lists[0][i][2][1])+'-'+str(crew_lists[0][i][2][2])+";;" 
+		s += crew_lists[1][i][0] + ";;;;;"+str(crew_lists[1][i][2][0])+";"+str(crew_lists[1][i][2][1])+'-'+str(crew_lists[1][i][2][2])+";;"
+		s += crew_lists[2][i][0] + ";" + str(crew_lists[2][i][1]) + ";;;;;"+str(crew_lists[2][i][2][0])+";"+str(crew_lists[2][i][2][1])+'-'+str(crew_lists[2][i][2][2])+";;"
+		s += crew_lists[3][i][0] + ";" + str(crew_lists[3][i][1]) + ";;;;;"+str(crew_lists[3][i][2][0])+";"+str(crew_lists[3][i][2][1])+'-'+str(crew_lists[3][i][2][2])+";;"
+		s += crew_lists[4][i][0] + ";" + str(crew_lists[4][i][1]) + ";;;;;"+str(crew_lists[4][i][2][0])+";"+str(crew_lists[4][i][2][1])+'-'+str(crew_lists[4][i][2][2])+";;"  
 		f.write(s + '\n')
 			
 
@@ -137,7 +237,7 @@ def write_headers(cast, crew):
 		crew.write(j + classifications)
 
 def main():
-	getTVdetailsby_year('2015')
+	getTVdetailsby_year('2016')
 
 if __name__ == '__main__':
 	main()
